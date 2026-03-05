7fc68b9e4360ba2f542bf8ec4acff3f3d18f301e
Adnan Saeed <adnansaeed.datascientist@gmail.com>
2026-03-05 17:05:38 +0500

    used deterministic approach

---
+++ CONFIG.py
@@ -0,0 +1,22 @@
+POSTGRES_USER='postgres_user'
+POSTGRES_PASSWORD='postgres_password'
+POSTGRES_DB='postgres_db'
+
+BASE_URL="http://localhost:12434"
+MODEL="llama3.2:1B-Q8_0"
+
+GROQ_MODEL='llama-3.3-70b-versatile'
+TEMPERATURE=0.7
+
+GEMINI_MODEL='gemini-2.5-flash-lite'
+
+PDFS = [
+    r"A:\PRO\p1\KaravanTech_Company_Policies.pdf",
+    r"A:\PRO\p1\KaravanTech_Company_Profile.pdf",
+    r"A:\PRO\p1\KaravanTech_Products_and_Pricing.pdf",
+]
+
+INDEX_PATH = "karavantech_faiss"
+
+EMBED_MODEL = "text-embedding-3-small"
+CHAT_MODEL = "gpt-4o-mini"
